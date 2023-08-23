6d96b22a2f41b1b78fa76046c92c4b8902d56b9e
Jerin <jerinnadar7@student.sfit.ac.in>
2023-08-24 00:31:55 +0530

    Initial commit

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,19 +1,26 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore, collection} from 'firebase/firestore'
+import {getFirestore,collection} from 'firebase/firestore'
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: "filmy-4e1c5.firebaseapp.com",
-  projectId: "filmy-4e1c5",
-  storageBucket: "filmy-4e1c5.appspot.com",
-  messagingSenderId: "206563926562",
-  appId: "1:206563926562:web:cb0f036f3426455472e07b"
+  apiKey: "AIzaSyCK2yj7qPHS2IjJR6bfLwZk-6mE3aTfeRc",
+  authDomain: "filmyverse-20ecf.firebaseapp.com",
+  projectId: "filmyverse-20ecf",
+  storageBucket: "filmyverse-20ecf.appspot.com",
+  messagingSenderId: "544067804175",
+  appId: "1:544067804175:web:880d3d294c5d88e939577a"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const moviesRef = collection(db, "movies");
-export const reviewsRef = collection(db, "reviews");
-export const usersRef = collection(db, "users");
 
-export default app;
+
+ export const db = getFirestore(app);
+ export const moviesRef = collection(db,"movies");
+ export const reviewsRef =collection(db,"reviews");
+ export const usersRef = collection(db, "users");
+ export default app;
